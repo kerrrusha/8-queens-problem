cc429d52dfb07557151830eff416a802c7fa433f
Kirill Koval <brcyrill2@gmail.com>
2022-12-05 22:43:12 +0200

    added copy constructor

--- src/main/java/com/kerrrusha/nqueenproblem/stat/AlgorithmNQueenStatTracker.java
+++ src/main/java/com/kerrrusha/nqueenproblem/stat/AlgorithmNQueenStatTracker.java
@@ -4,6 +4,15 @@ public class AlgorithmNQueenStatTracker extends AlgorithmStatTracker {
 
     private int backtracksCount = 0;
 
+    public AlgorithmNQueenStatTracker() {}
+
+    public AlgorithmNQueenStatTracker(AlgorithmNQueenStatTracker other) {
+        this.backtracksCount = other.backtracksCount;
+        this.steps = other.steps;
+        this.timeElapsedMillis = other.timeElapsedMillis;
+        this.stopwatch = other.stopwatch;
+    }
+
     @Override
     protected void reset() {
         backtracksCount = 0;
